865d8e7a2ba3abfec15155713cc9352901273391
eawebmaster20 <eaweb20@gmail.com>
2025-02-04 23:35:32 +0000

    feat: folder structure setup

---
+++ src/app/shared/components/main-toolbar/main-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-toolbar.component.html',
+  styleUrl: './main-toolbar.component.scss'
+})
+export class MainToolbarComponent {
+
+}
