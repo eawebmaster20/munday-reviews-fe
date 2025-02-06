4b42ad9b194fdcb8cbfa72596a6aa0bee46e6c38
eawebmaster20 <eaweb20@gmail.com>
2025-02-06 16:21:43 +0000

    feat: update auth service with initial endpoint

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/services/api/api.service';
+import { IAuthReqPayload } from '../../core/models/authReq.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private api: ApiService) {}
+
+  login(payload: IAuthReqPayload) {
+    return this.api.post('auth/login', payload);
+  }
 }
