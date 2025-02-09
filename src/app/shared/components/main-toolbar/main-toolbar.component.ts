fe185e21405ce109bec49820b058b42a76e7425f
eawebmaster20 <eaweb20@gmail.com>
2025-02-09 07:53:35 +0000

    feat: update main toobar with login in status

--- src/app/shared/components/main-toolbar/main-toolbar.component.ts
+++ src/app/shared/components/main-toolbar/main-toolbar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../features/auth/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-main-toolbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './main-toolbar.component.html',
   styleUrl: './main-toolbar.component.scss',
 })
-export class MainToolbarComponent {}
+export class MainToolbarComponent {
+  constructor(public authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
+}
