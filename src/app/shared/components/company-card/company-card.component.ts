1f28320642249f5a6dc6a4ce9eb8a37897e98a03
eawebmaster20 <eaweb20@gmail.com>
2025-02-09 07:54:20 +0000

    feat: add event emiter to company card component

--- src/app/shared/components/company-card/company-card.component.ts
+++ src/app/shared/components/company-card/company-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
 import { ICompanyCardData } from '../../../core/models/companycard.interface';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-company-card',
   standalone: true,
-  imports: [MatCardModule, StarRatingComponent],
+  imports: [MatCardModule, StarRatingComponent, MatButtonModule],
   templateUrl: './company-card.component.html',
   styleUrl: './company-card.component.scss',
 })
 export class CompanyCardComponent {
   @Input() data!: ICompanyCardData;
+  @Output() clickEvent: EventEmitter<ICompanyCardData> = new EventEmitter<ICompanyCardData>();
+
+  handleClick() {
+    this.clickEvent.emit(this.data);
+  }
 }
