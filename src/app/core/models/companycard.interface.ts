357904eed9d5b027e072b57a4f730d071d9a2257
eawebmaster20 <eaweb20@gmail.com>
2025-02-10 16:13:53 +0000

    feat: update company interface

--- src/app/core/models/companycard.interface.ts
+++ src/app/core/models/companycard.interface.ts
@@ -3,7 +3,30 @@ export interface ICompanyCardData {
   name: string;
   logoUrl: string;
   reviewsCount: number;
-  info: string;
+  reviews: IReview[];
+  description: string;
   averageRating: number;
   readonly: boolean;
+}
+
+export interface IReview {
+  companyId: number;
+  content: string;
+  createdAt: string;
+  id: number;
+  rating: number;
+  timestamp: string;
+  title: string;
+  updatedAt: string;
+  user: User;
+  userId: number;
+}
+
+interface User {
+  createdAt: string;
+  email: string;
+  id: number;
+  password: string;
+  updatedAt: string;
+  username: string;
 }
